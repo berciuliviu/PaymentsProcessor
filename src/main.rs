5b743949e89949437a11b3db0780c47d59be4524
Liviu Marian Berciu <liviu.berciu@sectorlabs.ro>
2022-05-29 13:42:00 +0300

    First project version

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::env;
+use toy_processor::processor::Processor;
+
+fn main() {
+    // Process arguments
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        eprintln!("There should be only one argument given to the program.");
+        std::process::exit(1);
+    }
+
+    let filename: String = match args.get(1) {
+        Some(file) => file.to_string(),
+        None => {
+            eprintln!("Error! No argument provided.");
+            std::process::exit(1);
+        }
+    };
+
+    let mut toy_processor: Processor = Processor::new(filename);
+    toy_processor.process_transactions();
+    toy_processor.print_clients().unwrap();
+}
---
+++ src/processor.rs
@@ -0,0 +1,115 @@
+use std::collections::HashMap;
+use std::error::Error;
+
+use crate::client::Client;
+use crate::transaction::{Transaction, TxType};
+
+pub struct Processor {
+    filename: String,
+    clients: HashMap<u16, Client>,
+}
+
+// Declare const headers with lazy_static so allocation is possible at
+// runtime https://docs.rs/lazy_static/latest/lazy_static/
+lazy_static! {
+    static ref FULL_HEADER: csv::ByteRecord =
+        csv::ByteRecord::from(vec!["type", "client", "tx", "amount"]);
+    static ref PARTIAL_HEADER: csv::ByteRecord =
+        csv::ByteRecord::from(vec!["type", "client", "tx"]);
+}
+
+/*******************************
+< Processor >
+
+Processes received CSV files into Client accounts hashmap.
+
+*******************************/
+impl Processor {
+    pub fn new(filename: String) -> Self {
+        Self {
+            filename: filename,
+            clients: HashMap::new(),
+        }
+    }
+    pub fn process_transactions(&mut self) {
+        // Create Builder from file
+        // - remove spaces
+        // - allow different length rows
+        let mut csv_reader = csv::ReaderBuilder::new()
+            .flexible(true)
+            .trim(csv::Trim::All)
+            .from_path(&self.filename)
+            .unwrap_or_else(|err| {
+                eprintln!(
+                    "Error when trying to read from CSV: {}, {}",
+                    self.filename, err
+                );
+                std::process::exit(1);
+            });
+
+        // Deserialize each row, based on headers length
+        for row in csv_reader.byte_records() {
+            if let Ok(result) = row {
+                let tx: Result<Transaction, csv::Error> = match result.len() {
+                    4 => result.deserialize(Some(&FULL_HEADER)),
+                    3 => result.deserialize(Some(&PARTIAL_HEADER)),
+                    _ => {
+                        eprintln!("Only rows with 3 or 4 fields are allowed.");
+                        continue;
+                    }
+                };
+                if let Err(error) = tx {
+                    eprintln!("Error when deserializing field: {}", error);
+                    continue;
+                }
+
+                if let Err(error) = self.process_transaction(tx.unwrap()) {
+                    eprintln!("{}", error);
+                }
+            }
+        }
+    }
+
+    pub fn process_transaction(&mut self, transaction: Transaction) -> Result<(), Box<dyn Error>> {
+        // We retrieve the client
+        // If he doesn't exist, we create a new one
+        let client_id: u16 = transaction.get_client_id();
+
+        let client: &mut Client = if let Some(client) = self.clients.get_mut(&client_id) {
+            client
+        } else {
+            self.clients.insert(client_id, Client::new(client_id));
+            self.clients.get_mut(&client_id).unwrap()
+        };
+
+        match transaction.get_tx_type() {
+            TxType::Deposit => {
+                client.consume_deposit(transaction)?;
+                client.add_transaction(transaction);
+            }
+
+            TxType::Withdrawal => {
+                client.consume_withdrawal(transaction)?;
+                client.add_transaction(transaction);
+            }
+
+            TxType::Dispute => client.consume_dispute(transaction)?,
+
+            TxType::Resolve => client.consume_resolve(transaction)?,
+
+            TxType::Chargeback => client.consume_chargeback(transaction)?,
+        }
+
+        Ok(())
+    }
+
+    pub fn print_clients(&self) -> Result<(), Box<dyn Error>> {
+        let mut writer = csv::Writer::from_writer(std::io::stdout());
+
+        for (_, client) in self.clients.iter() {
+            writer.write_byte_record(&csv::ByteRecord::from(client.record()))?;
+        }
+
+        Ok(())
+    }
+}
